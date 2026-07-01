0727bd4c81a06fe3d7b03eb025597b1fc1c100fd
mjkim <audwnssp@naver.com>
2026-07-01 14:40:33 +0900

    chore: OpenLayers 도입 및 webpack .env 주입/CSS 로더 설정

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: {
@@ -9,4 +10,14 @@ module.exports = {
     clean: true,
     filename: './js/app.js',
   },
+  module: {
+    rules: [
+      // OpenLayers 등의 CSS를 JS에서 import 할 수 있게 한다.
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+    ],
+  },
+  plugins: [
+    // .env 값을 빌드 시 process.env.* 로 주입한다. (.env 없어도 빌드는 진행)
+    new Dotenv({ systemvars: true }),
+  ],
 };
